2a349d796a8c842a22e2ac72cc425713163b796f
eduardo vinagre <eduardovinagre@gmail.com>
2026-04-15 06:46:59 -0600

    Class 8. Composition

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from typing import Protocol
+
+
+class LibroProtocol(Protocol):
+    def prestar(self) -> str:
+        ...
+
+    def devolver(self) -> str:
+        ...
+
+    def calcular_duracion(self) -> str:
+        ...
+
 class Libro:
     def __init__(self, titulo: str, autor, isbn: str, disponible: bool):
         self.titulo = titulo
@@ -32,15 +45,44 @@ class Libro:
         self.__veces_prestado = veces_prestado
 
 
+class LibroFisico(Libro):
+    def calcular_duracion(self):
+        return "7 dias"
+    
+
+class LibroDigital(Libro):
+    def calcular_duracion(self):
+        return "14 días"
+
+
+class Biblioteca:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.libros = []
+        self.usuarios = []
+
+    def libros_disponibles(self):
+        return [libro.titulo for libro in self.libros if libro.disponible]
+
+
 mi_libro = Libro(
     "100 años de soledad", "Gabriel Garcia Marquez", "978-8420471839", True
 )
 otro_libro = Libro("El Principito", "Saint-Exupéry", " 978-6073803762", True)
+otro_libro_2 = Libro("El arte de la guerra", "Sun Tzu", " " \
+"9798375541266", False)
 
-print(mi_libro.prestar())
-print(mi_libro.devolver())
+# print(mi_libro.prestar())
+# print(mi_libro.devolver())
 
-lista_libros = [mi_libro, otro_libro]
+# lista_libros = [mi_libro, otro_libro]
 
-for libro in lista_libros:
-    print(libro)
+# for libro in lista_libros:
+#     print(libro)
+
+biblioteca = Biblioteca("")
+biblioteca.libros.append(mi_libro)
+biblioteca.libros.append(otro_libro)
+biblioteca.libros.append(otro_libro_2)
+
+print(biblioteca.libros_disponibles())
